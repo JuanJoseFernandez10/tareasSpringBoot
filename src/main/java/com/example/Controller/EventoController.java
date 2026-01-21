78d015a18a06c84fd548c4e7370725569f81c5c2
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2026-01-21 14:39:30 +0100

    implementacion de metodos en el controlador(acabar vistas)

--- src/main/java/com/example/Controller/EventoController.java
+++ src/main/java/com/example/Controller/EventoController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.Entidad.Evento;
 import com.example.Service.EventoService;
 
 @Controller
@@ -18,8 +20,19 @@ public class EventoController {
 	@GetMapping
 	public String listarEventos(Model modelo) {
 		modelo.addAttribute("eventos", servicio.listarTodos());
-		
 		return "eventos/lista";
+	}
+	
+	@GetMapping("/eliminar")
+	public String eliminarEvento(@ModelAttribute Evento evento) {
+		servicio.eliminarEvento(evento);
+		return "evento/lista";
+	}
+	
+	@GetMapping("/guardar")
+	public String guardarEvento(@ModelAttribute Evento evento) {
+		servicio.guardarEvento(evento);
+		return "evento/lista";
 	}
 	
 }
